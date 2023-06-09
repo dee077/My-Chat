78d074fa81e3ad0b0fb1ace2948df79c3cee294c
Deepanshu <deepanshu.sahu27@gmail.com>
2023-06-10 00:19:59 +0530

    Deplyment Frontend

--- backend/server.js
+++ backend/server.js
@@ -37,7 +37,7 @@ app.use(notFound);
 app.use(errorHandler);
 
 const PORT=process.env.PORT || 5000
-const FRONT_URL= "http://localhost:3000";
+const FRONT_URL= "https://my-chat-gqf1.onrender.com";
 
 const server = app.listen(
     PORT,
